6e06d4bc77b9220f6970172f26afadd01aeaa3e9
Vignesh Sundaramahalingam <vignesh.sundaramahalingam@gmail.com>
2025-07-16 19:23:18 +0200

    test

--- test.py
+++ test.py
@@ -11,15 +11,13 @@ def replace_text_in_doc(file_path, placeholder, new_text, output_path):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print(len(sys.argv))
         print("Usage: python test.py <new_company_name>")
         sys.exit(1)
-    print(len(sys.argv))
     input_docx = "/app/AnschreibenRaw.docx"
     placeholder = "Company_name"  # e.g., Company_name    
     replacement = sys.argv[1]  # e.g., Alten
-    output_docx = "/app/Anschreiben.docx"
-    print(output_docx)
+    output_docx = "/app/Anschreiben_Vignesh.docx"
+    
 
     replace_text_in_doc(input_docx, "Company_name", replacement, output_docx)
     print("Replacement complete. Output saved to", output_docx)
