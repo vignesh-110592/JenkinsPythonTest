2ed9a1a415a003441205ba5d695efd9ee105299c
Vignesh Sundaramahalingam <vignesh.sundaramahalingam@gmail.com>
2025-07-17 22:24:07 +0200

    test

--- convert_docx_to_pdf.py
+++ convert_docx_to_pdf.py
@@ -1,11 +1,13 @@
 from docx2pdf import convert
 import os
 
-source = "C:/ProgramData/Jenkins/.jenkins/workspace/test/Anschreiben.docx"
+sourceAnschreiben = "C:/ProgramData/Jenkins/.jenkins/workspace/test/Anschreiben.docx"
+sourceLebenslauf = "C:/ProgramData/Jenkins/.jenkins/workspace/test/Lebenslauf.docx"
 dest = "C:/ProgramData/Jenkins/.jenkins/workspace/test/pdf_output"
 
 if not os.path.exists(dest):
     os.makedirs(dest)
 
-convert(source, dest)
+convert(sourceAnschreiben, dest)
+convert(sourceLebenslauf, dest)
 print("✅ Converted to PDF.")
