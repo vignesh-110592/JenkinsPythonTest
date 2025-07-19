eec92db854d02b921b9896a200d774a49b245a24
Vignesh Sundaramahalingam <vignesh.sundaramahalingam@gmail.com>
2025-07-19 13:05:50 +0200

    test

--- replace_docx_text.py
+++ replace_docx_text.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     
     # Process Lebenslauf if needed
     modified_timestamp = os.path.getmtime("Lebenslauf.docx")
-    modified_date = datetime.datetime.fromtimestamp(modified_timestamp).date()
+    modified_date = datetime.fromtimestamp(modified_timestamp).date()
     print(modified_date)
     # Compare dates
     if modified_date == current_date:
