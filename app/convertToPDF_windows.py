4e562984e3f30ba2336b5f41a0c9b69fb14ca2a5
Vignesh Sundaramahalingam <vignesh.sundaramahalingam@gmail.com>
2025-07-29 21:22:26 +0200

    updated folder structure

---
+++ app/convertToPDF_windows.py
@@ -0,0 +1,48 @@
+# convert_to_pdf_windows.py
+
+from docx2pdf import convert
+from datetime import datetime
+import os
+import sys
+from os.path import dirname
+
+source_files = ["Anschreiben.docx", "Lebenslauf.docx"]
+output_dir = "C:\\Users\\svvav\\Desktop\\Doc"
+
+os.makedirs(output_dir, exist_ok=True)
+current_date = datetime.today().strftime("%d.%m.%Y")
+modified_timestamp = os.path.getmtime(os.path.join(output_dir, "Lebenslauf.pdf"))
+modified_date = datetime.fromtimestamp(modified_timestamp).strftime("%d.%m.%Y")
+#print(modified_date)
+repodir= dirname(dirname(__file__))
+for file in source_files:   
+    #os.path.join(repodir, "output").exists()
+    file_path = os.path.join(repodir, "output", file)
+    if os.path.exists(file_path):
+
+        if file == "Lebenslauf.docx":
+            if modified_date == current_date :
+                print(f"File Lebenslauf.pdf was modified today. Skipping conversion.")
+                continue
+            else:
+                print(f"File Lebenslauf.pdf was last modified on {modified_date}. Attempting to convert {file}...")
+                try:
+                    convert(file, output_dir)
+                    print(f"Conversion successful: {file}")
+                except Exception as e:
+                    print(f"Error during conversion: {e}")
+                    sys.exit(1)
+        else:
+            print(f"Attempting to convert {file}...")
+            try:
+                convert(file, output_dir)
+                print(f"Conversion successful: {file}")
+            except Exception as e:
+                print(f"Error during conversion: {e}")
+                sys.exit(1)
+    else:
+        print(f"File not found: {file}")
+        sys.exit(1)
+
+
+
---
+++ app/verify_docx_text.py
@@ -0,0 +1,51 @@
+import sys
+import os
+from docx import Document
+from datetime import datetime
+from os.path import dirname
+
+def verify_placeholders(doc, expected_values):
+    text = "\n".join([para.text for para in doc.paragraphs])
+    for key, val in expected_values.items():
+        print(f"Verifying '{key}' with expected value '{val}' in document.")
+        if val not in text:
+            print(f"Verification failed: '{val}' not found.")
+            return False
+    return True
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: python verify_docx_text.py <CompanyName> <Greeting> <PersonName> <PositionName>")
+        sys.exit(1)
+
+    company_name = sys.argv[1]
+    Greeting = sys.argv[2]
+    person_name = sys.argv[3]
+    position_name = sys.argv[4]
+
+    current_date = datetime.today().strftime("%d.%m.%Y")
+
+    expected_anschreiben = {
+        "__DATE__": current_date,
+        "Company_name": company_name,
+        "Greeting": Greeting,
+        "Person_name": person_name,
+        "Position_name": position_name
+    }
+
+    expected_lebenslauf = {
+        "__DATE__": current_date
+    }
+    repodir= dirname(dirname(__file__))
+    #base_path = os.getcwd()
+
+    doc1 = Document(os.path.join(repodir, "output/Anschreiben.docx"))
+    doc2 = Document(os.path.join(repodir, "output/Lebenslauf.docx"))
+
+    if not verify_placeholders(doc1, expected_anschreiben):
+        sys.exit(1)
+    
+    if not verify_placeholders(doc2, expected_lebenslauf):
+        sys.exit(1)
+
+    print("Verification passed.")
